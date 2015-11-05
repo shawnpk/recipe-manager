8641a1b4b0a6d22f9959c81125225dc1e6038925
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-05 23:43:47 +0000

    Add login/logout functionality

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,26 @@
+class LoginsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    chef = Chef.find_by(email: params[:email])
+    
+    if chef && chef.authenticate(params[:password])
+      session[:chef_id] = chef.id
+      flash[:success] = 'You have logged in successfully'
+      redirect_to recipes_path
+    else
+      flash.now[:danger] = 'Your email address/password combination is incorrect'
+      render 'new'
+    end
+  end
+  
+  def destroy
+    session[:chef_id] = nil
+    flash[:success] = 'You have successfully logged out'
+    redirect_to root_path
+  end
+  
+end
