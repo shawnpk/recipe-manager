6d5ec853305c6957715aab00b1745e4d7bd78894
Shawn Kearney <shawnpk@ineptsoftware.com>
2015-11-05 19:02:56 +0000

    Add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104223418) do
+ActiveRecord::Schema.define(version: 20151105181946) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   create_table "likes", force: :cascade do |t|
